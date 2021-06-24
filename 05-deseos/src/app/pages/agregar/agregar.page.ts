6036d04c748f5d2e7d5b7d25ed0ee3d2502c93a2
andres97473 <andres97473@hotmail.com>
2021-06-24 16:54:19 -0500

    Commit 2021-06-24.17.00

---
+++ 05-deseos/src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
+import { ListaItem } from '../../models/lista-item.model';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage implements OnInit {
+
+  lista:Lista;
+
+  nombreItem='';
+
+  constructor( private deseosService:DeseosService,
+                private route: ActivatedRoute ){
+      const listaId = this.route.snapshot.paramMap.get('listaId');
+
+      console.log(listaId);
+
+      this.lista= this.deseosService.obtenerLista(listaId);
+
+      console.log(this.lista);     
+
+  }
+
+  ngOnInit() {
+  }
+
+  agregarItem(){
+
+    if( this.nombreItem.length === 0){
+      return;
+    }
+    
+    const nuevoItem = new ListaItem(this.nombreItem);
+    this.lista.items.push( nuevoItem );
+
+    this.deseosService.guardarStorage();
+    this.nombreItem = '';
+
+  }
+
+  cambioCheck(item: ListaItem ){
+    
+    const pendientes = this.lista.items.filter( itemData => !itemData.completado).length;
+
+    console.log({pendientes});
+
+    if( pendientes === 0 ){
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    }
+    else{
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+
+    }
+
+    console.log(this.deseosService.listas);
+    
+    
+
+    this.deseosService.guardarStorage();
+    
+  }
+
+  borrar ( i: number ){
+    this.lista.items.splice( i, 1 );
+    this.deseosService.guardarStorage();
+  }
+
+}
--- 05-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
-import { Lista } from '../../models/lista.model';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,8 +11,55 @@ import { Lista } from '../../models/lista.model';
 export class Tab1Page {
 
 
-  constructor( public deseosService: DeseosService ) {
+  constructor( public deseosService: DeseosService,
+                private router: Router,
+                private alertController: AlertController ) {
 
   }
+
+  async agregarLista(){
+
+    // this.router.navigateByUrl('tabs/tab1/agregar');
+
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name:'titulo',
+          type:'text',
+          placeholder:'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+        text:'Cancelar',
+        role:'cancel',
+        handler: ()=> {
+          console.log('Cancelar');          
+        }
+      },
+      {
+        text: 'Crear',
+        handler: (data)=> {
+          console.log(data);
+          if( data.titulo.length===0)
+          {
+            return;
+          }
+
+          // si el largo no es 0 tengo que crear la lista
+
+          const listaId = this.deseosService.crearLista(data.titulo);
+
+          this.router.navigateByUrl(`tabs/tab1/agregar/${ listaId }`);
+        }
+      }
+    ]
+    });
+
+    alert.present();
+
+  }  
 
 }
