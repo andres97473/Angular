28a8a16bb1d0624c95cd2f383137406a010e6e69
andres97473 <andres97473@hotmail.com>
2021-07-06 16:51:51 -0500

    Commit 2021-07-06.14.50

---
+++ 06-miscelaneos/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 06-miscelaneos/src/app/directives/resaltado.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]'
+})
+export class ResaltadoDirective {
+
+    
+  constructor( private el:ElementRef) { 
+    console.log("Directiva llamada");
+    // el.nativeElement.style.backgroundColor = "yellow";
+        
+  }
+  
+
+  @HostListener('mouseenter') mouseEntro(){
+    this.el.nativeElement.style.backgroundColor = "yellow";
+  }
+
+  @HostListener('mouseleave') mouseSalio(){
+    this.el.nativeElement.style.backgroundColor = null;
+  }
+
+}
