8f264b70760b49918515767458c5b419ce037e8a
andres97473 <andres97473@hotmail.com>
2021-07-28 12:22:08 -0500

    Commit 2021-07-28.12.22

--- 08-formularios/src/app/services/validadores.service.ts
+++ 08-formularios/src/app/services/validadores.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { timeout } from 'rxjs/operators';
+
+interface ErrorValidate {
+  [s:string]: boolean
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +13,25 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ValidadoresService {
 
   constructor() { }
+
+  existeUsuario( control: FormControl ):Promise<any> | Observable<any>{
+
+    if( !control.value){
+      return Promise.resolve(null);  
+    }
+
+    return new Promise ( (resolve, reject)=>{
+      setTimeout( ()=>{
+
+        if( control .value === 'strider' ){
+          resolve({ existe: true });
+        } else {
+          resolve ( null );
+        }
+
+      }, 3500);
+    });
+  }
 
   noHerrera( control: FormControl ){
 
