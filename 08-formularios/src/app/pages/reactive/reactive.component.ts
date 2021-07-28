2d2c75bc3fad207b10adae2a7103417240debc9d
andres97473 <andres97473@hotmail.com>
2021-07-27 19:28:33 -0500

    Commit 2021-07-27.19.28

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -12,19 +13,22 @@ export class ReactiveComponent implements OnInit {
   forma: FormGroup;
   
 
-  constructor( private fb: FormBuilder ) { 
+  constructor( private fb: FormBuilder, 
+                private validadores:ValidadoresService ) { 
 
     this.forma = this.fb.group({
       nombre: ['',[Validators.required, Validators.minLength(5)]],
-      apellido: ['',[Validators.required, Validators.minLength(5)]],
+      apellido: ['',[Validators.required, Validators.minLength(5), this.validadores.noHerrera ]],
       correo: ['',[Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      pass1:['',Validators.required],      
+      pass2:['',Validators.required],      
       direccion: this.fb.group({
         distrito: ['', Validators.required],
         ciudad: ['', Validators.required]
       }),
-      pasatiempos: this.fb.array([
-        [],[],[]
-      ])
+      pasatiempos: this.fb.array([ ])
+    },{
+      validators: this.validadores.passwordIguales('pass1','pass2')
     });
 
     this.cargarDataAlFormulario();
@@ -59,6 +63,17 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
   }
 
+  get pass1NoValido(){
+    return this.forma.get('pass1')?.invalid && this.forma.get('pass1')?.touched
+  }
+  
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1')?.value;
+    const pass2 = this.forma.get('pass2')?.value;
+
+    return ( pass1 === pass2 ) ? false : true;
+  }
+
 
   cargarDataAlFormulario(){
 
@@ -72,6 +87,18 @@ export class ReactiveComponent implements OnInit {
           ciudad: "pasto"
         }
     });
+
+  }
+
+  agregarPasatiempo(){
+
+    this.pasatiempos.push( this.fb.control('', Validators.required ));
+    
+  }
+
+  borrarPasatiempo(index: number){
+
+    this.pasatiempos.removeAt(index);
 
   }
 
