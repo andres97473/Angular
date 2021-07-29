3ef65fa3e9ee84efd1c701cbdf523931696de046
andres97473 <andres97473@hotmail.com>
2021-07-28 20:31:51 -0500

    Commit 2021-07-28.20.31

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -3,6 +3,9 @@ import { NgForm } from '@angular/forms';
 import { HeroeModel } from '../../models/heroe.model';
 import { HeroesService } from '../../services/heroes.service';
 
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -24,22 +27,36 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando informacion',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>
+
     // console.log( form );
     // console.log( this.heroe );
 
     if( this.heroe.id ){
 
-      this.heroesService.actualizarHeroe(this.heroe).subscribe( resp =>{
-        console.log( resp );      
-      });
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
       
     } else {
 
-      this.heroesService.crearHeroe(this.heroe).subscribe( resp =>{
-        console.log( resp );      
-      });
+      peticion = this.heroesService.crearHeroe(this.heroe);
 
     }
+
+    peticion.subscribe( resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizo correctamente',
+        icon: 'success'
+      });
+    });
 
     
   }
--- 09-heroesApp/src/app/services/heroes.service.ts
+++ 09-heroesApp/src/app/services/heroes.service.ts
@@ -34,4 +34,31 @@ export class HeroesService {
 
     return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTemp );
   }
+
+  getHeroes(){
+    return this.http.get(`${ this.url }/heroes.json`)
+                .pipe(
+                  map( resp => this.crearArreglo(resp))
+                );
+  }
+
+  private crearArreglo( heroesObj: object | any ){
+
+    const heroes : HeroeModel[] = [];
+
+    
+    if( heroesObj === null ){ return []; }
+
+    Object.keys ( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+
+      heroes.push( heroe );
+      
+    }); 
+
+    return heroes;
+
+  }
+
 }
