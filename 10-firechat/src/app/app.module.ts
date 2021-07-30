8d4789afefea690e901479a35d85527927938592
andres97473 <andres97473@hotmail.com>
2021-07-29 21:06:42 -0500

    Commit 2021-07-29.21.06

--- 10-firechat/src/app/app.module.ts
+++ 10-firechat/src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
 import { AngularFireModule } from'@angular/fire';
 import { AngularFirestoreModule } from'@angular/fire/firestore';
 import { AngularFireAuthModule } from'@angular/fire/auth';
 
 import { environment } from '../environments/environment';
 import { ChatComponent } from './components/chat/chat.component';
+
+// services
+import { ChatService } from './providers/chat.service';
 
 
 @NgModule({
@@ -22,9 +27,12 @@ import { ChatComponent } from './components/chat/chat.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 10-firechat/src/app/components/chat/chat.component.ts
+++ 10-firechat/src/app/components/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../providers/chat.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-chat',
@@ -8,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string="";
+  elemento: any;
 
-  ngOnInit(): void {
+  constructor( public _cs: ChatService ) { 
+    this._cs.cargarMensajes().subscribe( () => {
+
+                              setTimeout( () => {
+                                
+                                this.elemento.scrollTop = this.elemento.scrollHeight;
+
+                              }, 20)
+                             });
+  }
+
+  ngOnInit(){
+
+    this.elemento = document.getElementById('app-mensajes');
+
+  }
+
+  enviarMensaje( mensaje: string ){
+
+    console.log(this.mensaje);
+
+    if ( this.mensaje.length === 0 ){
+
+      return;
+
+    } else {
+
+      this._cs.agregarMensaje(this.mensaje)
+          .then( ()=> {console.log('Mensaje enviado')
+                        this.mensaje=""})
+          .catch( (err)=>console.error('Error al guardar', err));
+
+    }
+    
+
   }
 
 }
