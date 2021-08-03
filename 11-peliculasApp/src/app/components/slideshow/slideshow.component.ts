110d1bfe9be89b282f2d5c143f9cc36883121b74
andres97473 <andres97473@hotmail.com>
2021-08-02 20:49:47 -0500

    Commit 2021-08-02.20.50

--- 11-peliculasApp/src/app/components/slideshow/slideshow.component.ts
+++ 11-peliculasApp/src/app/components/slideshow/slideshow.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 import { Movie } from '../../interfaces/cartelera-response';
 
 @Component({
@@ -6,15 +7,35 @@ import { Movie } from '../../interfaces/cartelera-response';
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[]=[]; 
 
+  public mySwiper: any=undefined;
+
   constructor() { }
 
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container', {
+      loop: true,    
+    });    
+  }
+
   ngOnInit(): void {
-    console.log( this.movies );
+    // console.log( this.movies );
     
+  }
+
+  onSlideNext(){
+
+    this.mySwiper.slideNext();
+
+  }
+
+  onSlidePrev(){
+
+    this.mySwiper.slidePrev();
+
   }
 
 }
