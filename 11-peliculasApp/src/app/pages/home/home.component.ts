4d6a1d50dedc67eb9215058c7a9eec43adf01a40
andres97473 <andres97473@hotmail.com>
2021-08-04 16:59:49 -0500

    Commit 2021-08-04.17.00

--- 11-peliculasApp/src/app/pages/home/home.component.ts
+++ 11-peliculasApp/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Movie } from '../../interfaces/cartelera-response';
 
@@ -11,6 +11,32 @@ import { Movie } from '../../interfaces/cartelera-response';
 export class HomeComponent implements OnInit {
 
   public movies: Movie[]=[];
+  public moviesSlideshow: Movie[]=[];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(){
+    
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop ) + 1300 ;
+    const max = (document.documentElement.scrollHeight || document.body.scrollHeight );
+
+    // console.log({pos, max});
+
+    if ( pos > max) {
+
+      // console.log('Llamar el servicio');
+      if( this._ps.cargando){
+        return;
+      }
+
+      this._ps.getCartelera().subscribe( movies => {
+        this.movies.push(...movies);
+
+      });
+      
+
+    }
+
+  }
 
   constructor( private _ps: PeliculasService ) { 
       
@@ -19,16 +45,18 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
     this._ps.getCartelera().subscribe(
-      resp=>{
+      movies=>{
         // todo el objeto
         // console.log(resp); 
 
         // array de peliculas
-        this.movies=resp.results;
+        this.movies=movies;
         //console.log( this.movies ); 
 
         // imprimir titulo del primer elemnto del array
-        // console.log(resp.results[0].title);       
+        // console.log(resp.results[0].title);    
+        
+        this.moviesSlideshow = movies;
       });
   }
 
--- 11-peliculasApp/src/app/services/peliculas.service.ts
+++ 11-peliculasApp/src/app/services/peliculas.service.ts
@@ -1,18 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { Observable, of } from 'rxjs';
+import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
+
+import { tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
 
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+  public cargando: boolean = false;
+
   constructor( private http: HttpClient) { }
 
-  getCartelera():Observable<CarteleraResponse> {
-
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=954b2ee1864d402ba1bd247b574257c0&language=es-ES&page=1')
-  
+  get params(){
+    return {
+      api_key:'954b2ee1864d402ba1bd247b574257c0',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    }
   }
+
+  getCartelera():Observable<Movie[]> {
+
+    if ( this.cargando ){
+      return of([]);
+    }
+
+    this.cargando = true;
+
+    return this.http.get<CarteleraResponse>(`${ this.baseUrl }/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      map( (resp) => resp.results ),
+      tap( () => {
+        this.carteleraPage +=1;
+        this.cargando = false; 
+      })
+    );
+  }
+
+  buscarPeliculas( texto: string ):Observable<Movie[]>{
+
+    const params = {...this.params, page:'1', query: texto };
+
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/search/movie`, {
+      params: params
+    }).pipe(
+          map(resp => resp.results)
+      )
+  }
+
+
+
 }
