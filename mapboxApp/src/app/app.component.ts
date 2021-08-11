3f067a8a209a768206e1327fb73000bdf78f65b7
andres97473 <andres97473@hotmail.com>
2021-08-11 16:57:18 -0500

    Commit 2021-08-11.17.00

---
+++ mapboxApp/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import * as Mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  mapa: Mapboxgl.Map | undefined;
+
+  longitud = -77.57292232200311;
+  latitud = 0.8068493949669744;
+
+
+  ngOnInit(): void {
+
+    ( Mapboxgl as any ).accessToken = environment.mapboxKey;
+
+    this.mapa = new Mapboxgl.Map({
+      container: 'mapa-mapbox', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [ this.longitud, this.latitud ], // LNG, LAT
+      zoom: 16.6
+    }); 
+
+    this.crearMarcador( this.longitud, this.latitud );
+
+  }
+
+  
+  crearMarcador(lng: number, lat: number ){
+
+    const marker = new Mapboxgl.Marker({
+      draggable: true
+      }).setLngLat([ lng, lat ])
+        .addTo( (this.mapa as any ) );
+
+    marker.on('dragend', () => {
+      console.log( marker.getLngLat() );
+      
+    });
+
+  }
+
+
+
+
+}
---
+++ mapboxApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxKey: 'pk.eyJ1IjoiYW5kcmVzOTc0NzMiLCJhIjoiY2tzN3lkcHM5M21xOTJ2bzdvOTV5ZzNwdiJ9.KWIl8OJyULENIfjsGO1ThA'
+};
