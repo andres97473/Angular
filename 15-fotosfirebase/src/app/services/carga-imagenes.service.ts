8883ffe0556663e316b2a0ac9307359fe9156465
andres97473 <andres97473@hotmail.com>
2021-08-17 23:37:15 -0500

    Commit 2021-08-17.23.37

--- 15-fotosfirebase/src/app/services/carga-imagenes.service.ts
+++ 15-fotosfirebase/src/app/services/carga-imagenes.service.ts
@@ -4,6 +4,8 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import firebase from "firebase/app";
 import { FileItem } from '../models/file-item';
 
+import 'firebase/storage';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,12 +18,7 @@ export class CargaImagenesService {
 
    }
 
-   private guardarImagen( imagen: { nombre:string, url: string } ){
-
-    this.db.collection(`/${ this.CARPETA_IMAGENES}`)
-            .add( imagen );
-
-   }
+   
 
    cargarImagenesFirebase( imagenes:FileItem[] ){
 
@@ -40,16 +37,30 @@ export class CargaImagenesService {
                                                     .put( item.archivo );
 
       uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
-        (snapshop:firebase.storage.UploadTaskSnapshot )=>item.progreso = ( snapshop.bytesTransferred / snapshop.totalBytes ) * 100,
+        (snapshop:firebase.storage.UploadTaskSnapshot )=>
+        item.progreso = ( snapshop.bytesTransferred / snapshop.totalBytes ) * 100,
           (error) => console.error('Error al subir ', error),
           ()=>{
             console.log('Imagen cargada correctamente');
-            item.url = uploadTask.snapshot.downloadURL;
             
-          }        
-        )
+            uploadTask.snapshot.ref.getDownloadURL().then( resp => {
+              //console.log(resp);
+              this.guardarImagen( {
+                nombre:item.nombreArchivo,
+                url:resp
+              } );              
+            });                       
+            
+            item.estaSubiendo = false;           
+                      
+          });
     }
-    
+   }
+
+   private guardarImagen( imagen: { nombre:string, url: string } ){
+
+    this.db.collection(`/${ this.CARPETA_IMAGENES }`)
+            .add( imagen );
 
    }
 
--- 15-fotosfirebase/src/environments/environment.prod.ts
+++ 15-fotosfirebase/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCZnnYLA8UgDXWmhjfyLcjGr23xWPnKKoc",
+    authDomain: "fir-fotos-7fab3.firebaseapp.com",
+    projectId: "fir-fotos-7fab3",
+    storageBucket: "fir-fotos-7fab3.appspot.com",
+    messagingSenderId: "974656701919",
+    appId: "1:974656701919:web:0ba1bce41ae74f4667706a",
+    measurementId: "G-7S4W37E8ED"
+  }
 };
