62168731f2dce10116a41136fe4c2c07864eb068
andres97473 <andres97473@hotmail.com>
2021-11-16 16:38:31 -0500

    Commit 2021-11-16.16.39

--- angularDesignicode/src/app/data/constants/user.const.ts
+++ angularDesignicode/src/app/data/constants/user.const.ts
@@ -6,7 +6,8 @@ export const USERS_DATA: ICardUser[] = [
     name: 'Ivan Drago',
     age: 28,
     description: 'El ruso',
-    avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png',
+    avatar:
+      'https://www.milenio.com/uploads/media/2020/09/29/actor-dolph-lundgren-encargo-interpretar.jpg',
     work: 'Boxeador',
   },
   {
@@ -14,21 +15,24 @@ export const USERS_DATA: ICardUser[] = [
     name: 'Rocky Balboa',
     age: 26,
     description: 'El garañon italiano',
-    avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png',
+    avatar:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rocky-1628776091.jpeg',
   },
   {
     id: 3,
     name: 'Apolo Creed',
     age: 31,
     description: 'Boxeador',
-    avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png',
+    avatar:
+      'https://statics.viralizalo.com/virs/2017/02/VIR_350431_26173_que_boxeador_de_la_saga_rocky_eres.jpg',
   },
   {
     id: 4,
     name: 'Mr. T',
     age: 32,
     description: 'Boxeador',
-    avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png',
+    avatar:
+      'https://as01.epimg.net/epik/imagenes/2020/03/06/portada/1583495941_496253_1583496039_noticia_normal_recorte1.jpg',
   },
   {
     id: 5,
--- angularDesignicode/src/app/modules/user/user-detail/user-detail.component.ts
+++ angularDesignicode/src/app/modules/user/user-detail/user-detail.component.ts
@@ -10,14 +10,16 @@ import { USERS_DATA } from '../../../data/constants/user.const';
 })
 export class UserDetailComponent implements OnInit {
   public id: number;
-
   public users: ICardUser[] = USERS_DATA;
+  public currentUser?: ICardUser;
 
   constructor(private route: ActivatedRoute) {
     this.id = this.route.snapshot.params['id'];
+    this.currentUser = this.users.find((user) => user.id === +this.id);
+    //console.log(this.currentUser);
   }
 
   ngOnInit(): void {
-    console.log(this.users);
+    // console.log(this.users);
   }
 }
