aa6396373272c66e70cf80fac63c3410db98ae9d
andres97473 <andres97473@hotmail.com>
2021-11-22 21:30:41 -0500

    Commit 2021-11-22.21.30

--- angularDesignicode/src/app/modules/user/user-list/user-list.component.ts
+++ angularDesignicode/src/app/modules/user/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ICardUser } from '../../../shared/components/cards/card-user/card-user.metadata';
 import { USERS_DATA } from '../../../data/constants/user.const';
 import { UserService } from '../../../data/services/api/user.service';
@@ -10,7 +10,7 @@ import { CAROUSEL_DATA_ITEMS } from '@data/constants/carousel.const';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, AfterViewInit {
   cargado = false;
   public users!: ICardUser[];
   public carouselData: ICarouselItem[] = CAROUSEL_DATA_ITEMS;
@@ -33,4 +33,6 @@ export class UserListComponent implements OnInit {
       }
     });
   }
+
+  ngAfterViewInit() {}
 }
