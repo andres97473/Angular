65e658a75f4b71c6913e0c7d7f7f098ca88d1bf1
andres97473 <andres97473@hotmail.com>
2023-07-24 20:28:20 -0500

    Croppep funciona

--- crop-image/src/app/image-crop-uploader/image-crop-uploader.component.ts
+++ crop-image/src/app/image-crop-uploader/image-crop-uploader.component.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
+import {
+  ImageCroppedEvent,
+  LoadedImage,
+  base64ToFile,
+} from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-image-crop-uploader',
@@ -8,31 +13,52 @@ import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
   styleUrls: ['./image-crop-uploader.component.scss'],
 })
 export class ImageCropUploaderComponent {
+  selectedFile: File | null = null;
+
   imageChangedEvent: any = '';
   croppedImage: any = '';
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
   fileChangeEvent(event: any): void {
     this.imageChangedEvent = event;
   }
+
   imageCropped(event: ImageCroppedEvent) {
     if (event.objectUrl) {
       this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(
         event.objectUrl
       );
+
       // event.blob can be used to upload the cropped image
     }
+    if (event.blob) {
+      this.selectedFile = new File([event.blob], 'imageName.jpeg', {
+        type: 'image/jpeg',
+      }); // Ajusta el tipo de imagen si es necesario
+    }
   }
-  imageLoaded(image: LoadedImage) {
-    console.log(image);
 
-    // show cropper
-  }
-  cropperReady() {
-    // cropper ready
-  }
-  loadImageFailed() {
-    // show message
+  sendFile(): void {
+    if (this.selectedFile) {
+      console.log(this.selectedFile);
+
+      const formData = new FormData();
+      formData.append('idAlbum', '64b1e1836671cfe212376669');
+      formData.append('avatar', this.selectedFile);
+      formData.append('type', 'cover'); // Ajusta el tipo de imagen adecuadamente
+
+      // Ajusta la URL para que apunte al endpoint en tu servidor que recibirá la imagen
+      const url = 'http://localhost:5000/images/avatar';
+
+      this.http.post(url, formData).subscribe(
+        (response) => {
+          console.log('Imagen enviada exitosamente:', response);
+        },
+        (error) => {
+          console.error('Error al enviar la imagen:', error);
+        }
+      );
+    }
   }
 }
